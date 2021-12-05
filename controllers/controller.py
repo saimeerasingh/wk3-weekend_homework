1600dbca7930ab7eab7bd97b5684024dd4f4219f
saimeerasingh <singhsaimeera@gmail.com>
2021-12-05 21:13:49 +0000

    added methods

--- controllers/controller.py
+++ controllers/controller.py
@@ -8,5 +8,14 @@ from models.items import *
 def index():
     return render_template('index.html', title = 'Shopping List',items_list=items_list)
     
+@app.route('/items',methods=['POST'])
+def add_item():
+    new_item_name = request.form['name']
+    new_item_price = request.form['price']
+    new_item_quantity = request.form['quantity']
+    new_item_bought = True if 'bought' in request.form else False
+    new_item_list = Items(new_item_name,new_item_price,new_item_quantity,new_item_bought)
+    add_new_item(new_item_list)
+    return redirect ('/items')
 
-
+@app.route()
--- models/items_list.py
+++ models/items_list.py
@@ -1,7 +1,10 @@
 from models.items import *
 
 item1 = Items('Cookie',2,3,True)
-item2 = Items('Tea',3,2,Fale)
+item2 = Items('Tea',3,2,False)
 item3 = Items('Milk',4,1,True)
-item_list = [item1,item2,item3]
+items_list = [item1,item2,item3]
+
+def add_new_item(item):
+    items_list.append(item)
 
